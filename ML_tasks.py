1a21165d6621c87ae6a16efaeff94091ec74bb48
MacyL <rorotira@gmail.com>
2016-07-26 19:16:07 +0200

    Update ML_tasks.py

--- ML_tasks.py
+++ ML_tasks.py
@@ -81,10 +81,8 @@ Task1_Bigram_Ans=np.sum(LRModel_accuracy)/10
 print(Task1_Bigram_Ans)
 0.8195
 ############# Kares : follow tips from exercise 5 ###############################
+# function from exercise 5
 def train_test(model, x, y, folds):
-    """ This function trains and tests a Keras model with k-fold cv. 
-        'folds' is the array returned by sklearn *KFold splits.
-    """
     acc_sum = 0
     for trn_i, test_i in folds:
         model.fit(x[trn_i], y[trn_i], nb_epoch=1)
@@ -102,13 +100,129 @@ for i in range(len(X)):
 	else:
 		y[i]=[False] # negative
 Y_matrix=np.asmatrix(y)
-single_layer = Sequential()
-single_layer.add(Dense(output_dim=1, input_dim=X_matrix.shape[1], activation='sigmoid'))
-single_layer.compile(loss='binary_crossentropy',optimizer='sgd', metrics=['accuracy'])
+my_single_layer = Sequential()
+my_single_layer.add(Dense(output_dim=1, input_dim=X_matrix.shape[1], activation='sigmoid'))
+my_single_layer.compile(loss='binary_crossentropy',optimizer='sgd', metrics=['accuracy'])
+# 10 fold cross validation
 folds = StratifiedKFold(Y, 10)
-single_layer_accuracy = train_test(single_layer, X_matrix, Y_matrix, folds)
+my_single_layer_accuracy = train_test(my_single_layer, X_matrix, Y_matrix, folds)
 print(single_layer_accuracy.mean())
 0.6285
+# Bigram, 'memory error' during traforming to array, stop using bigram so far. 
 ##########This result is suspecious, because there is a huge difference between sklearn and keras ########
+################################## clear all the histroy, and then move on to task 2 #####################
+# Task 2 : MLP, word vector
+# The strategy. For this task, use word vector to replace the token counts.
+# The word vector downloaded Glove 6B word vector, I use 50 Dimension because my computer capacity.
+# Because Sklearn hasn't release 0.8 version, I used Sknn for MLP. Also try Keras. 
+###########################################################################################################
+WordVector=open("/home/admin1/Documents/Machine_learning/assignment3/glove_6B/glove.6B.50d.txt").readlines()
+#read in as list
+myDictionary = dict()
+for each in WordVector:
+	each=each.split()
+	key=each[0]
+	vec=np.array([float(tmp) for tmp in each[1:]])
+	myDictionary[key]=vec
+	
+FullData=pd.read_csv("/home/admin1/Documents/Machine_learning/assignment3/Full_Data.csv")
+FullData=FullData.drop(FullData.columns[[0]], axis=1)
+X=FullData['text']
+Y=FullData['isPos']
+# For this task, I try to tokenize by myself, get the data frame with texts as rows and tokens as columns
+# NLTK regular expression tokenizer
+tokenizer= RegexpTokenizer(r'\w+|\$[\d\.]+|\S+')
+FullDoc=[]
+for each in X:
+	Unigram = tokenizer.tokenize(each)
+	Unigram_counter=Counter(Unigram)
+	df=[]
+	# give the word vector some weights, not just simply replace it. 
+	for k in Unigram_counter:
+		if k in myDictionary:
+			miniWordVector=myDictionary[k]*Unigram_counter[k]
+			df +=[miniWordVector]
+	WordVectorSum=np.mean(df,axis=0)
+	FullDoc +=[WordVectorSum]
+
+Y=pd.DataFrame(Y)
+FullDocDataFrame=pd.DataFrame(FullDoc)
+FullDocDataFrame=pd.concat([Y,FullDocDataFrame],axis=1)
+# output to csv file so I can start from here in the future
+FullDocDataFrame.to_csv("Task2_Data_Unigram.csv")
+# Bigram
+Full100D=[]
+for each in X:
+	Unigram = tokenizer.tokenize(each)
+	Bigram = list(ngrams(Unigram,2))
+	#The strategy is split the bigram into two keys, build their own vector sets calculate average and then concate the final vectors together. 
+	Bigramkey1=[i[0] for i in Bigram]
+	BigramKey1Count=Counter(Bigramkey1)
+	BigramKey1minivec=[myDictionary[k]*BigramKey1Count[k] for k in BigramKey1Count if k in myDictionary]
+	BigramKey1mean=np.mean(BigramKey1minivec,axis=0)	
+	# key2 
+	BigramKey2=[i[1] for i in Bigram]
+	BigramKey2Count=Counter(BigramKey2)
+	BigramKey2minivec=[myDictionary[k]*BigramKey2Count[k] for k in BigramKey2Count if k in myDictionary]
+	BigramKey2mean=np.mean(BigramKey2minivec,axis=0)
+	#print(myBigramKey1mean, myBigramKey2mean)
+	# Concate
+	BigramVec=np.concatenate((BigramKey1mean,BigramKey2mean), axis=0)
+	Full100D+=[BigramVec]
+
+Y=pd.DataFrame(Y)
+Full100DDataFrame=pd.DataFrame(Full100D)
+Full100DDataFrame=pd.concat([Y,Full100DDataFrame],axis=1)
+Full100DDataFrame.to_csv("Task2_Data_Bigram.csv")
+################################################# 
+Task2=pd.read_csv("Task2_Data_Unigram.csv")
+Task2=Task2.drop(Task2.columns[[0]], axis=1)
+#Seperate X and Y
+Y=Task2['isPos']
+X=Task2[Task2.columns[1:51]]
+# Transform X into matrix is very important
+X_matrix = X.as_matrix()
+# Set up MLP 
+MLP = Classifier(
+    layers=[
+        Layer("Sigmoid", units=200),
+        Layer("Softmax")],
+    learning_rate=0.02,
+    n_iter=10)
+    
+MLP_accuracy = cross_val_score(MLP, X_matrix, Y, cv=10, scoring='accuracy')
+Task2_Unigram_Ans=np.sum(MLP_accuracy)/10
+print(Task2_Unigram_Ans)
+0.664
+# I also tried units with 50, 100, 150, 200
+# The answer is 0.646, 0.6465, 0.6405
+Task2=pd.read_csv("Task2_Data_Bigram.csv")
+Task2=Task2.drop(Task2.columns[[0]], axis=1)
+#Seperate X and Y
+Y=Task2['isPos']
+X=Task2[Task2.columns[1:101]]
+X_matrix = X.as_matrix()
+MLP = Classifier(
+    layers=[
+        Layer("Sigmoid", units=200),
+        Layer("Softmax")],
+    learning_rate=0.02,
+    n_iter=10)
+
+MLP_accuracy = cross_val_score(MLP, X_matrix, Y, cv=10, scoring='accuracy')
+Task2_Bigram_Ans=np.sum(MLP_accuracy)/10
+print(Task2_Bigram_Ans)
+0.6725
+# I also tried units with 50, 100, 150, 250
+# The answer is 0.629, 0.678, 0.6375, 0.6405,
+####################### clear all the history and move on to task 3 ############################
+# Task 3 : Convolutional neural network
+# For this task, use word vector to replace each token. 
+################################################################################################
+Data=pd.read_csv("/home/admin1/Documents/Machine_learning/assignment3/Full_Data.csv")
+Data=Data.drop(Data.columns[[0]], axis=1)
+X=Data['text']
+Y=Data['isPos']
+
 
 
